96be6571b3afff0915ecf5abe85de162c2889b00
Radivoje Peskic <radivoje.peskic@nutanix.com>
2025-08-05 16:02:31 +0200

    add dynamic config for backend and exposing port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8080',
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': env.POLL_API_HOST ?? 'http://localhost:8080',
+      },
+      port: env.PORT,
+      host: '0.0.0.0'
     },
-  },
+  }
 })
